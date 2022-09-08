13d96aeb2aced65d7b61b752a6691bfd5484e998
Shelumiel <sssharon7@gmail.com>
2022-09-08 21:19:12 +0800

    Add components

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form className="todo-form">
+        <input className="todo-input"></input>
+        <button className="todo-button">Add Items</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
