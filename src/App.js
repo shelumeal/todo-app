9d0ee8495b9468cbbf45c4e0859f909351881e70
Shelumiel <sssharon7@gmail.com>
2022-09-09 09:16:50 +0800

    add changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
-import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="todo-app">
-      <h2>Todo list</h2>
-      <Form></Form>
-    </div>
-  );
+  return <div className="todo-app"></div>;
 }
 
 export default App;
